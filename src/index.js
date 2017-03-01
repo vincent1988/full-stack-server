056102920dcdec726b1a3924ddede75b7cd71dd9
qiuhong <qiuhong@noahwm.com>
2017-03-01 14:58:43 +0800

    koa-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import koa from 'koa';
+import Router from 'koa-router';
 
 const app = koa();
+const router = new Router({prefix: '/user'});
 
 app.use(function *(next) {
 	const start = new Date;
@@ -11,37 +13,47 @@ app.use(function *(next) {
 
 app.name = 'server';
 
-app.use(function *(next) {
-  const start = new Date;
-  yield next;
-  const ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-  console.log(this.request.charset);
-  // this.throw(403);
+// app.use(function *(next) {
+//   const start = new Date;
+//   yield next;
+//   const ms = new Date - start;
+//   console.log('%s %s - %s', this.method, this.url, ms);
+//   console.log(this.request.charset);
+//   // this.throw(403);
+// });
+
+router.get('/', function *(next) {
+	this.body = 'Hello World!';
+}).get('/:path', function *(next) {
+	this.body = this.params.path;
 });
 
-app.use(function *() {
-  // this.redirect('http://google.com');
-  // this.cookie.set('aaa', 'test');
-	console.log(this.request.ip);
-	
-	this.set('ETag', '123');
-	if (this.fresh) {
-		this.status = 304;
-		return;
-	}
-	this.body = 'Hello World';
-	this.message = 'good';
-	console.log(this.get('ETag') + this.type);
-  // this.status = 301;
-  // this.redirect('/cart');
-  // this.body = 'Redirecting to shopping cart';
-});
+app
+	.use(router.routes())
+	.use(router.allowedMethods());
+// app.use(function *() {
+//   // this.redirect('http://google.com');
+//   // this.cookie.set('aaa', 'test');
+// 	console.log(this.request.ip);
+//
+// 	this.set('ETag', '123');
+// 	if (this.fresh) {
+// 		this.status = 304;
+// 		return;
+// 	}
+// 	this.body = 'Hello World';
+// 	this.message = 'good';
+// 	console.log(this.get('ETag') + this.type);
+//   // this.status = 301;
+//   // this.redirect('/cart');
+//   // this.body = 'Redirecting to shopping cart';
+// });
 
 app.on('error', (err, ctx) => {
 	console.log('===server error===', err, ctx);
 });
 
 
-app.listen(3800);
-app.listen(3100);
+app.listen(3800, function() {
+	console.log('listen 3800');
+});
